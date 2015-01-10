6aeed2b697c3bb3b6e263458cf7d0d7c97f24071
Declan Kelly <dkelly.home@gmail.com>
2015-01-09 19:27:26 -0800

    Made problem 5 more efficient

--- e5.hs
+++ e5.hs
@@ -1,5 +1,20 @@
 --Project Euler Problem 5 -- Smallest Multiple
 
---Inefficient, marked incomplete
-answer = take 1 $ filter (allDivis [2,3,5,7,9,11,12,13,16,17,19,20]) [1..]
-	where allDivis xs x = all (\a -> x `mod` a == 0) xs
+import Data.List (groupBy, sort, group)
+import Control.Applicative
+
+data Factor = Factor Int Int deriving (Show, Eq, Ord)
+
+answer :: Int
+answer = product . map (\(Factor a b) -> a ^ b) $ map last . groupBy (\(Factor a b) (Factor c d) -> a == c) . sort . concat . map factorMult $ map primeFactors [1..20]
+
+factorMult :: [Int] -> [Factor]
+factorMult xs = factorMult' $ group xs where
+	factorMult' [] = []
+	factorMult' (y:ys) = Factor (head y) (length y) : factorMult' ys
+
+primeFactors :: Integral t => t -> [t]
+primeFactors 1 = []
+primeFactors x =
+        let res = head (filter (\ v -> gcd x v == v) [2..x])
+        in res:primeFactors(x `div` res)
